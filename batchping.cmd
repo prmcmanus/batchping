589518ddcc09750d5fd662c9318a959a1037ab42
Paul McManus <24816842+prmcmanus@users.noreply.github.com>
2023-01-26 21:51:50 +0000

    Added OneDrive Kill/Launch and more
    Added OneDrive Kill Launch (for one version - it's the same on all the machines I'm working with right now)
    Added Current IP info
    Added Connected Hardware Info (WLAN, LAN, and VPN tested)
    Removed Pause at end so this can be added to Task Scheduler for my use case
    
    BUG Stupid first-ping-fail remains

--- batchping.cmd
+++ batchping.cmd
@@ -1,33 +1,62 @@
 @echo Off
-title Batch Ping
+title Ping Servers
 color 17
 cls
-echo *******************
-echo * Batch Ping Test *
-echo *******************
+echo **************************
+echo * Paul's Batch Ping Test *
+echo **************************
 echo.
 
-echo WARNING
-echo This will overwrite the results file results.csv
-pause
-
-:: Added some echos for each step to help identify any issues
-
-:: If error retrieving this file, check it's not open
-:: Excel stops access
 echo Getting Server List...
 Set "ServerList=servers.csv"
 echo Done
+echo.
 
-:: Same again, make sure it's not open in Excel
 echo Setting Results Location...
-Set "LogFile=results.csv"
+set dt=%DATE:~6,4%_%DATE:~3,2%_%DATE:~0,2%__%TIME:~0,2%_%TIME:~3,2%_%TIME:~6,2%
+set dt=%dt: =0%
+set LogFile=Results\Results-%dt%.csv
 echo Done
+echo.
+
+echo Killing OneDrive to prevent sync issues
+start "OneDrive" /D "C:\Program Files\Microsoft OneDrive\" OneDrive.exe /shutdown
+timeout 3 > NUL
+echo Done
+echo.
+
+echo Adding Current Connection Details
+echo My Current IP Details > %LogFile%
+ipconfig|Find "IPv4">>%LogFile%
+echo. >> %LogFile%
+echo. >> %LogFile%
+echo. >> %LogFile%
+echo Name                                                                              NetConnectionID               NetConnectionStatus  NetEnabled>> %LogFile%
+wmic NIC get Name,NetConnectionID,NetConnectionStatus,NetEnabled | Find "TRUE" >> %LogFile%
+echo. >> %LogFile%
+echo. >> %LogFile%
+echo. >> %LogFile%
+echo Done
+echo.
 
 echo Running Tests...
-If Not Exist "%ServerList%" Exit /B
->"%LogFile%" (For /F "tokens=1-2 delims=," %%A In (%ServerList%
-) Do Ping -n 1 %%A|Find "TTL=">Nul&&(Echo %%A,%%B,Pass)||Echo %%A,%%B,FAILED)
+echo The first ping test always fails >> %LogFile%
+echo Recommend a blank firstline in %ServerList% >> %LogFile%
+echo. >> %LogFile%
+echo. >> %LogFile%
+echo. >> %LogFile%
+echo Pass or Fail,IP Address or URL,Description >> %LogFile%
+>>"%LogFile%" (For /F "tokens=1-2 delims=," %%A In (%ServerList%) Do Ping -n 1 %%A|Find "TTL=">Nul&&(Echo Pass,%%A,%%B )||Echo FAIL,%%A,%%B )
 color 27
 echo Done
-pause
+echo.
+
+echo Starting OneDrive to sync new files
+start "OneDrive" /D "C:\Program Files\Microsoft OneDrive\" OneDrive.exe /background
+echo Done
+echo.
+
+color 20
+echo Finished
+timeout 3 > NUL
+color
