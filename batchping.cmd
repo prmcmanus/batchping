416e76950c4ef2de0564a8cfb91dea6761d5fd86
Paul McManus <24816842+prmcmanus@users.noreply.github.com>
2023-03-22 22:20:49 +0000

    That stupid patch where you do loads of things in massive patch like an idiot
    Fixed the bug around failing the first line
    Tidied up the output
    Removed a lot of messages and just added comments instead
    Added counter to show progress
    Added pass/fail counter

--- batchping.cmd
+++ batchping.cmd
@@ -1,62 +1,84 @@
-@echo Off
+@echo off
+setlocal EnableDelayedExpansion
 title Ping Servers
-color 17
 cls
 echo **************************
 echo * Paul's Batch Ping Test *
 echo **************************
 echo.
+echo Running...
 
-echo Getting Server List...
-Set "ServerList=servers.csv"
-echo Done
-echo.
-
-echo Setting Results Location...
-set dt=%DATE:~6,4%_%DATE:~3,2%_%DATE:~0,2%__%TIME:~0,2%_%TIME:~3,2%_%TIME:~6,2%
-set dt=%dt: =0%
-set LogFile=Results\Results-%dt%.csv
-echo Done
-echo.
-
-echo Killing OneDrive to prevent sync issues
+:: Killing OneDrive to prevent sync issues
 start "OneDrive" /D "C:\Program Files\Microsoft OneDrive\" OneDrive.exe /shutdown
 timeout 3 > NUL
-echo Done
-echo.
 
-echo Adding Current Connection Details
-echo My Current IP Details > %LogFile%
-ipconfig|Find "IPv4">>%LogFile%
-echo. >> %LogFile%
-echo. >> %LogFile%
-echo. >> %LogFile%
-echo Name                                                                              NetConnectionID               NetConnectionStatus  NetEnabled>> %LogFile%
-wmic NIC get Name,NetConnectionID,NetConnectionStatus,NetEnabled | Find "TRUE" >> %LogFile%
-echo. >> %LogFile%
-echo. >> %LogFile%
-echo. >> %LogFile%
-echo Done
-echo.
+:: Get the current date and time in the format YYYYMMDD-HHMM
+set "year=%date:~6,4%"
+set "month=%date:~3,2%"
+set "day=%date:~0,2%"
+set "hour=%time:~0,2%"
+set "minute=%time:~3,2%"
+if not exist Results mkdir Results
+set "filename=Results\Results-%year%%month%%day%-%hour%%minute%.csv"
 
-echo Running Tests...
-echo The first ping test always fails >> %LogFile%
-echo Recommend a blank firstline in %ServerList% >> %LogFile%
-echo. >> %LogFile%
-echo. >> %LogFile%
-echo. >> %LogFile%
-echo Pass or Fail,IP Address or URL,Description >> %LogFile%
->>"%LogFile%" (For /F "tokens=1-2 delims=," %%A In (%ServerList%) Do Ping -n 1 %%A|Find "TTL=">Nul&&(Echo Pass,%%A,%%B )||Echo FAIL,%%A,%%B )
-color 27
-echo Done
-echo.
+:: Set the input and output file names
+set "input_file=addresses.csv"
+set "output_file=%filename%"
 
-echo Starting OneDrive to sync new files
+:: Count the number of lines in the input file
+set "line_count=0"
+for /f "tokens=1" %%a in ('type "%input_file%" ^| find /v /c ""') do set "line_count=%%a"
+echo Number of lines in %input_file%: %line_count%
+
+:: Adding current IPv4s for this device
+echo IPv4 Addresses>>%output_file%
+for /f "tokens=2 delims=:" %%a in ('ipconfig^|find "IPv4 Address"') do (
+    set "ip=%%a"
+    echo !ip:~1!>>%output_file%
+)
+echo.>>%output_file%
+echo.>>%output_file%
+
+:: Adding currently used network devices
+echo Network Devices in use>>%output_file%
+for /f "tokens=1 delims=" %%a in ('wmic NIC get Name^,NetEnabled ^| find "TRUE"') do (
+    set "device=%%a"
+    set "device=!device:TRUE=!"
+    set "device=!device:  =!"
+    echo !device!>>%output_file%
+)
+echo.>>%output_file%
+echo.>>%output_file%
+
+:: Write the header to the output file
+echo Status,Address,Description >> "%output_file%"
+
+:: Loop through each line in the input file
+set "counter=0"
+set "pass_count=0"
+set "fail_count=0"
+for /f "tokens=1,2 delims=," %%a in (%input_file%) do (
+    set /a "counter+=1"
+    set "address=%%a"
+    set "description=%%b"
+
+    :: Ping the address and write the result to the output file
+    ping -n 1 !address! >nul
+    if errorlevel 1 (
+        echo FAIL,!address!,!description! >> "%output_file%"
+        set /a "fail_count+=1"
+    ) else (
+        echo Pass,!address!,!description! >> "%output_file%"
+        set /a "pass_count+=1"
+    )
+
+    :: Print a message to the console indicating the progress
+    echo Processed !counter! of %line_count% lines. Pass-!pass_count! Fail-!fail_count!
+)
+
+:: Restarting OneDrive
 start "OneDrive" /D "C:\Program Files\Microsoft OneDrive\" OneDrive.exe /background
-echo Done
-echo.
 
-color 20
-echo Finished
+:: Print a message to the console indicating where the results were saved
+echo Results saved in %output_file%
 timeout 3 > NUL
-color
